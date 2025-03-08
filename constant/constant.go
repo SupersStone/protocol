2e99496fbc0f41f3fef2c3f82acf07efe97c5f70
kxx <806544660@qq.com>
2025-03-08 15:37:06 +0800

    新增

--- constant/constant.go
+++ constant/constant.go
@@ -146,17 +146,11 @@ const (
 	DefalutNotKick = 0
 	// Full-end login, but the same end is mutually exclusive.
 	AllLoginButSameTermKick = 1
-	// Only one of the endpoints can log in.
-	SingleTerminalLogin = 2
-	// The web side can be online at the same time, and the other side can only log in at one end.
-	WebAndOther = 3
 	// The PC side is mutually exclusive, and the mobile side is mutually exclusive, but the web side can be online at
 	// the same time.
 	AllLoginButSameClassKick = 4
 	// The PC terminal can be online at the same time,but other terminal only one of the endpoints can login.
 	PCAndOther = 5
-	// Customize login policy
-	Customize = 6
 
 	// OnlineStatus  = "online"
 	// OfflineStatus = "offline"
@@ -304,6 +298,7 @@ const (
 	AppOrdinaryUsers     = 1
 	AppAdmin             = 2
 	AppNotificationAdmin = 3
+	AppRobotAdmin        = 4
 
 	GroupOwner         = 100
 	GroupAdmin         = 60
